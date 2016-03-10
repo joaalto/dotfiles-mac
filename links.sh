f83d833699e03667010e0eced7cf604be5031c2d
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-03-10 22:26:10 +0200

    Added .zshrc

--- links.sh
+++ links.sh
@@ -3,5 +3,6 @@ ln -s ~/dotfiles-mac/.aliases ~
 ln -s ~/dotfiles-mac/.mjolnir ~
 ln -s ~/dotfiles-mac/.tmux.conf ~
 ln -s ~/dotfiles-mac/.vimrc ~
+ln -s ~/dotfiles-mac/.zshrc ~
 
 # Link git installed by homebrew!
