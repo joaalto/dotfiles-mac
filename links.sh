2b0c38147a6a80295159063414bcc5742cb067ad
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-04-09 11:08:14 +0300

    Link .gitconfig file.

--- links.sh
+++ links.sh
@@ -6,5 +6,6 @@ ln -sf ~/dotfiles-mac/.tmux.conf ~
 ln -sf ~/dotfiles-mac/.vimrc ~
 ln -sf ~/dotfiles-mac/.zshrc ~
 ln -sf ~/dotfiles-mac/.atom/keymap.cson ~/.atom
+ln -sf ~/dotfiles-mac/.gitconfig ~
 
 # Link git installed by homebrew!
