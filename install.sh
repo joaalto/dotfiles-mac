d1e4ce18a0157bac0da649bc7654e422785fcf5a
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-06-13 23:10:59 +0300

    Add several cask apps.

--- install.sh
+++ install.sh
@@ -6,7 +6,14 @@ brew install httpie
 brew install lua
 brew install caskroom/cask/brew-cask
 
+brew cask install alfred
+brew cask install atom
+brew cask install google-chrome
 brew cask install hammerspoon
+brew cask install iterm2
+brew cask install karabiner
+brew cask install seil
+brew cask install slack
 
 ~/dotfiles-mac/links.sh
 
