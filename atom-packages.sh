f6043c8acb87faf4ef9390b24a2ce6e017a7996a
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-08-17 21:49:19 +0300

    Install Atom packages.

---
+++ atom-packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apm install vim-mode
+apm install keyboard-localization
+apm install language-elm
+apm install linter
+apm install linter-elm-make
+apm install elm-format
+apm install save-commands
+
